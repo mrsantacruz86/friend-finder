27269cd14d1903fc8c3bb6c49513d6c5b9629dea
Nelson Diaz <abnerdc86@gmail.com>
2018-04-24 17:17:12 -0400

    updatin html and data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,18 @@
 var data = [{
-	"name": "Sarah",
-	"photo": "https://photos.app.goo.gl/SqKGKWDlMkb1rbIF2",
+	"name": "Mountain Queen",
+	"photo": "https://cdn.pixabay.com/photo/2018/04/21/14/17/raise-3338589_960_720.jpg",
 	"scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
 }, {
-	"name": "manatee",
-		"photo": "https://en.wikipedia.org/wiki/Manatee#/media/File:FL_fig04.jpg",
+	"name": "Manatee",
+		"photo": "https://cdn.pixabay.com/photo/2017/04/13/01/04/manatee-2226320_960_720.jpg",
 	"scores": [5, 1, 4, 5, 5, 1, 1, 5, 2, 1]
 }, {
-	"name": "fulano",
-	"photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+	"name": "Cute Boy",
+	"photo": "https://cdn.pixabay.com/photo/2018/04/21/18/05/scarf-3339049_960_720.jpg",
 	"scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
 }, {
-	"name": "El vecino",
-	"photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+	"name": "El Perro",
+	"photo": "https://cdn.pixabay.com/photo/2018/04/08/13/39/dog-3301255_960_720.jpg",
 	"scores": [5, 1, 3, 4, 4, 1, 2, 1, 4, 5]
 }
 ];
